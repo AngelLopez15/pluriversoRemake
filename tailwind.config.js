bffc397a9c3002a4d29bbaab0d5f5d9c3a3eb806
Angel Lopez <a.o.lopez.cruz@gmail.com>
2021-04-27 00:10:48 -0500

    paleta de colores y media breakpoints agregados

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,22 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'subtitle': '#8FDE7D'
+        'subtitle': '#8FDE7D',
+        'secondary': '#78DEA7',
+        'action': '#68DEC8'
       }
+    },
+    screens: {
+      '2xl': { 'max': '1535px' },
+      // => @media (max-width: 1535px) { ... }
+      'xl': { 'max': '1279px' },
+      // => @media (max-width: 1279px) { ... }
+      'lg': { 'max': '1023px' },
+      // => @media (max-width: 1023px) { ... }
+      'md': { 'max': '767px' },
+      // => @media (max-width: 767px) { ... }
+      'sm': { 'max': '639px' },
+      // => @media (max-width: 639px) { ... }
     },
   },
   variants: {
