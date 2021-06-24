89c6ef73b312fa7ab22fc800fb5b7901c2ee84af
Angel Lopez <a.o.lopez.cruz@gmail.com>
2021-06-23 23:57:02 -0500

    Blog agregado a navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -127,6 +127,9 @@ export default function Navbar() {
               </li>
             ))
           }
+          <li>
+            <a className="px-4 font-semibold text-xl md:py-4" target="_blank" rel="noopener noreferrer" href="https://pluriversodigital.medium.com/">Blog</a>
+          </li>
         </ul>
       </div>
     </nav>
